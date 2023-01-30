388e6935816ba7beab11264e73f69af6fea39479
Victor <uberman59@gmail.com>
2023-01-31 01:35:22 +0500

    viteconfig fix

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue3-course/',
+  base: '/Vue3-course/',
   plugins: [vue()],
   resolve: {
     alias: {

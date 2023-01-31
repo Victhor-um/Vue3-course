ec041eba32b91c03cd1c89fb174a628a40181ddb
Victor <uberman59@gmail.com>
2023-01-31 10:48:48 +0500

    delete dist folder

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vue3-course/',
+  base: '/vue3-course/',
   plugins: [vue()],
   resolve: {
     alias: {
--- vue.config.js
+++
@@ -1,5 +0,0 @@
-// файл vue.config.js должен быть расположен в корневом каталоге проекта
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue3-course/' : '/',
-};

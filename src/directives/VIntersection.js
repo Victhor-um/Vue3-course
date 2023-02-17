4442f24ad48684670e941136940bf83bdae4f07a
Victor <uberman59@gmail.com>
2023-02-17 17:50:11 +0500

    fix: фиксы мелкие

--- src/directives/VIntersection.js
+++ src/directives/VIntersection.js
@@ -1,5 +1,8 @@
 export default {
   mounted(el, binding) {
+    if (typeof binding.value !== 'function') {
+      throw Error('Binding.value is not a function');
+    }
     const options = {
       rootMargin: '0px',
       threshold: 1.0,
